bd572c50c28abcec2a52b463802e0fb876bc0baa
personal <personal@Varun>
2017-07-11 23:57:17 +0530

    Slideshow - Pooja Varun

--- WebContent/controllers/app.js
+++ WebContent/controllers/app.js
@@ -46,14 +46,41 @@ routerApp.controller('widgetsController', function($scope,$http) {
 		$scope.name="Fail";
 		$scope.age=null;
 	});
+	
+	
+	
 });
 
 routerApp.controller('Myctrl', function($scope, $location) {
     $scope.isActive = function(route) {
         return route === $location.path();
-    }    
+        
+    }
+    var myIndex = 0;
+    carousel();
+
+    function carousel() {
+        var i;
+        var x = document.getElementsByClassName("mySlides");
+        var dots = document.getElementsByClassName("demo");
+        for (i = 0; i < x.length; i++) {
+           x[i].style.display = "none";  
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" w3-white", "");
+         }
+        myIndex++;
+        if (myIndex > x.length) {myIndex = 1}    
+        x[myIndex-1].style.display = "block";  
+        dots[myIndex-1].className += " w3-white";
+        setTimeout(carousel, 2000); // Change image every 2 seconds
+    }
 });
 
 /*$(document).on('scroll', function (e) {
     $('.navbar').css('opacity', ($(document).scrollTop() / 200));
 });*/
+
+
+
+//Slideshow Script
