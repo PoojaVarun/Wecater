6401bff27e4465ce0f212e58b4c0eeeb7dd7414b
personal <personal@Varun>
2017-07-19 22:22:32 +0530

    REST changes By Varun 22:22 19/07/2017

---
+++ src/catersAPI/com/CatersAPI.java
@@ -0,0 +1,26 @@
+package catersAPI.com;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.google.gson.Gson;
+
+import catersBean.User;
+
+@Path("/Caters")
+public class CatersAPI {
+	@GET
+	@Path("/getUsers")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getMsg() {
+		Gson gson=new Gson();
+		User user=new User("Varun", 22);
+		String s1=gson.toJson(user);
+		System.out.println(s1);
+		return Response.ok(s1).build();
+
+	}
+}
